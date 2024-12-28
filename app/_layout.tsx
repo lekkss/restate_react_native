610f9cd239dccbe414ebde32c6108dd82b853693
lekkss <leksguy97@gmail.com>
2024-12-28 17:59:08 +0000

    feat: appwrite integration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -19,5 +19,5 @@ export default function RootLayout() {
     }
   }, [fontsLoaded]);
   if (!fontsLoaded) return null;
-  return <Stack />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,48 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
 
 const SignIn = () => {
+  const handleLogin = () => {};
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="px-10">
+          <Text className="text-balance text-center uppercase font-rubik text-black-200">
+            Welcome to Restate
+          </Text>
+          <Text className="font-rubik-bold mt-2 text-center text-black-300 text-3xl">
+            Lets Get You Closer to {"\n"}{" "}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
+          <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+            Login to ReState with Google
+          </Text>
+          <TouchableOpacity
+            className="bg-white shadow-md w-full rounded-full shadow-zinc-300 py-4 mt-4"
+            onPress={handleLogin}
+          >
+            <View className="flex flex-row justify-center items-center ">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
