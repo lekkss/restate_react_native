9e7a4db21c5ef807a9e359db775ebbfcf974f9b9
lekkss <leksguy97@gmail.com>
2024-12-28 18:03:25 +0000

    feat: useAppwrite hook

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,28 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
+import { login } from "@/lib/appwrite";
 
 const SignIn = () => {
-  const handleLogin = () => {};
+  const handleLogin = async () => {
+    const result = await login();
+    if (result) {
+      console.log("login successful");
+    } else {
+      Alert.alert("errot", "failed to login");
+    }
+  };
+
+  
   return (
     <SafeAreaView className="bg-white h-full">
       <ScrollView contentContainerClassName="h-full">
